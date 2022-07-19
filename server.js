5bf9673509dced0bfd3120b339cb825f31fe8bef
Rahul <rahulkarda2002@gmail.com>
2022-07-19 20:13:24 +0530

    Update PORT for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 require('dotenv').config()
 
 
