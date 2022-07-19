0b44b0970d18ecce8ef5f300c2846e90e39fab5e
Rahul <rahulkarda2002@gmail.com>
2022-07-19 19:23:06 +0530

    Update PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 3000
+const PORT = process.env.port || 3000
 require('dotenv').config()
 
 
@@ -84,6 +84,6 @@ app.delete('/deleteQuote', (request, response) => {
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
